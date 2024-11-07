73fea8655a3b727e3d8cdd8e15070a730ea0f20c
Marco Iorio <marco.iorio@isovalent.com>
2024-11-07 18:02:32 +0100

    endpoint: silence metadata resolver not found errors during restoration
    [ backporter's notes: additionally applied the filter to the occurrence in
      UpdateVisibilityPolicy, which got removed in the main branch. ]
    
    During the agent bootstrap phase, it is possible to attempt restoring an
    endpoint for a pod that has just been deleted asynchronously, but the
    endpoint deletion event has not been received (as it has been queued
    through the deletion file). If this race condition occurs, the metadata
    resolution tasks would fail, because the pod does no longer exist, in
    turn leading to error logs such as:
    
        level=error msg="EndpointNoTrackEvent event failed"
    
    Let's lower the severity of these messages to info level, given that the
    endpoint is going to be deleted anyways. Indeed, we are restoring an
    existing endpoint, whose corresponding pod had existed in the past, and
    the cache is guaranteed to be synced. The pod must have been deleted in
    the meanwhile in case we are not seeing it anymore.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- pkg/endpoint/endpoint.go
+++ pkg/endpoint/endpoint.go
@@ -20,6 +20,7 @@ import (
 	"github.com/cilium/hive/cell"
 	"github.com/sirupsen/logrus"
 	"github.com/vishvananda/netlink"
+	k8sErrors "k8s.io/apimachinery/pkg/api/errors"
 	k8sTypes "k8s.io/apimachinery/pkg/types"
 
 	"github.com/cilium/cilium/api/v1/models"
@@ -1760,6 +1761,15 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 		return false, nil
 	}
 
+	filterResolveMetadataError := func(err error) error {
+		if restoredEndpoint && k8sErrors.IsNotFound(err) {
+			e.getLogger().WithError(err).Info("Unable to resolve metadata during endpoint restoration. Is the pod still running?")
+			return nil
+		}
+
+		return err
+	}
+
 	// copy the base labels into this local variable
 	// so that we don't override 'baseLabels'.
 	controllerBaseLabels := labels.NewFrom(baseLabels)
@@ -1769,6 +1779,10 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	pod, k8sMetadata, err := resolveMetadata(ns, podName)
 	switch {
 	case err != nil:
+		if filterResolveMetadataError(err) == nil {
+			break
+		}
+
 		e.Logger(resolveLabels).WithError(err).Warning("Unable to fetch kubernetes labels")
 		fallthrough
 	case e.K8sUID != "" && e.K8sUID != string(pod.GetUID()):
@@ -1800,7 +1814,7 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	e.UpdateNoTrackRules(func(_, _ string) (noTrackPort string, err error) {
 		po, _, err := resolveMetadata(ns, podName)
 		if err != nil {
-			return "", err
+			return "", filterResolveMetadataError(err)
 		}
 		value, _ := annotation.Get(po, annotation.NoTrack, annotation.NoTrackAlias)
 		return value, nil
@@ -1808,7 +1822,7 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	e.UpdateVisibilityPolicy(func(_, _ string) (proxyVisibility string, err error) {
 		po, _, err := resolveMetadata(ns, podName)
 		if err != nil {
-			return "", err
+			return "", filterResolveMetadataError(err)
 		}
 		value, _ := annotation.Get(po, annotation.ProxyVisibility, annotation.ProxyVisibilityAlias)
 		return value, nil
@@ -1816,7 +1830,7 @@ func (e *Endpoint) metadataResolver(ctx context.Context,
 	e.UpdateBandwidthPolicy(bwm, func(ns, podName string) (bandwidthEgress string, err error) {
 		_, k8sMetadata, err := resolveMetadata(ns, podName)
 		if err != nil {
-			return "", err
+			return "", filterResolveMetadataError(err)
 		}
 		return k8sMetadata.Annotations[bandwidth.EgressBandwidth], nil
 	})
